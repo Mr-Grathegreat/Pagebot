1cf9d9381135984e00ebcaf197186fca9ee57590
Slim Shady <oyamatmot86@gmail.com>
2025-01-25 16:28:58 +0800

    Update get.js

--- modules/scripts/commands/get.js
+++ modules/scripts/commands/get.js
@@ -1,11 +1,11 @@
 module.exports.config = {
-  name: "get", // Command Name (IMPORTANT)
-  author: "Yan Maglinte", // The author of this script
+  name: "uid", // Command Name (IMPORTANT)
+  author: "Mot", // The author of this script
   version: "1.0", // If you want to update your own version, please update this.
   category: "Utility", // Change this to the desired category, helpful for help.js command
   description: "Sends the user's recipient ID", // Command's description
-  adminOnly: true, // Only admins can use this command
-  usePrefix: true, // Will use a PREFIX if its true to activate this command
+  adminOnly: false, // Only admins can use this command
+  usePrefix: false, // Will use a PREFIX if its true to activate this command
   cooldown: 0, // cooldown time in seconds
 };
 
@@ -14,12 +14,12 @@ module.exports.config = {
 module.exports.run = function ({ event, args }) {
   // If event type is just a message
   if (event.type === "message") {
-    api.sendMessage("This is your ID: " + event.sender.id, event.sender.id);
+    api.sendMessage("" + event.sender.id, event.sender.id);
   };
 
   // The received ID, can be used in the config.json to add your ID as part of the admin list and will be sent if it's replying to a message
   if (event.type === "message_reply") {
-    api.sendMessage(`This your ID: ${event.sender.id}`, event.sender.id);
+    api.sendMessage(`${event.sender.id}`, event.sender.id);
     console.log(event)
   }
   // This will send a message to the sender if it replies to a message
